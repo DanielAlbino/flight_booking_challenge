385b87a49b7ab048b159ce6b63f9ccec5c928242
Daniel Albino <daniel18albino@gmail.com>
2023-10-26 15:52:44 +0100

    configure tailwind and typescript

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import BookingView from "./Views/booking-view/";
+
+function App() {
+  return (
+    <div className="App">
+      <BookingView />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Views/booking-view/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchBar from "../../Components/Searchbar/";
+
+
+const BookingView = () => {
+    return <SearchBar/>
+}
+
+
+export default BookingView;
